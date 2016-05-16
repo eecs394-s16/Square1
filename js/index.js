0a859c3033ccd53d0352ba689f4d638fd8611995
PENGYUAN <pengyuan2015@u.northwestern.edu>
2016-05-16 16:25:33 -0500

    solveNestedProblem and show inventory table

--- js/index.js
+++ js/index.js
@@ -9,22 +9,18 @@ $( document ).ready(function() {
 function load(id) {
   switch(id) {
     case "dash":
-      jQuery.get('pages/dash.html', function(data) {
-        document.getElementById("page-wrapper").innerHTML = data;
-        jQuery.get('pages/inventory-widget.html', function(data2) {
-          document.getElementById("inventory-panel").innerHTML = data2;
-          jQuery.get('pages/schedule-widget.html', function(data3) {
-            document.getElementById("schedule-panel").innerHTML = data3;
-            loadFirebase(id);
-          });
-        });
-      });
-      break;
-    case "inventory":
-      jQuery.get('pages/inventory.html', function(data) {
-        document.getElementById("page-wrapper").innerHTML = data;
+  
+      $("#page-wrapper").load('pages/dash.html', function(data) {
+        $('#inventory-panel').load('pages/inventory-widget.html');
+        $('#schedule-panel').load('pages/schedule-widget.html');
         loadFirebase(id);
       });
+      break;
+
+    case "inventory":
+  
+      $('#page-wrapper').load('pages/inventory.html');
+      loadFirebase(id);
       break;
     case "schedule":
       jQuery.get('pages/schedule.html', function(data) {
@@ -55,28 +51,29 @@ function loadFirebase(id){
       // INVENTORY
       // -------------------------------------------------------------------------
       var itemsRef = new Firebase('https://square1.firebaseio.com');
-      itemsRef.on("value", function(snapshot) {
+      itemsRef.once("value", function(snapshot) {
         // alert(snapshot.val());  // Alerts "San Francisco"
         // document.getElementById("demo").innerHTML = snapshot.val();
         var table = document.getElementById("itemsTable");
         snapshot.forEach(function(data) {
-        var newItem = data.val();
-        var row = table.insertRow(0);
-        // table.setAttribute("align","center");
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
-        var cell7 = row.insertCell(6);
-        var cell8 = row.insertCell(7);
-        var cell9 = row.insertCell(8);
-        var cell10 = row.insertCell(9);
-        cell1.innerHTML = newItem.Part;
-        cell2.innerHTML = newItem.Sourcing.Cost;
-        cell3.innerHTML = newItem.Sourcing.Inventory;
-        cell4.innerHTML = '<a href ='+newItem.Sourcing.Link+' style="text-decoration:none"> <button class="btn btn-secondary">Order</button></a>'
+          
+          var newItem = data.val();
+          var row = table.insertRow(0);
+          // table.setAttribute("align","center");
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
+          var cell3 = row.insertCell(2);
+          var cell4 = row.insertCell(3);
+          var cell5 = row.insertCell(4);
+          var cell6 = row.insertCell(5);
+          var cell7 = row.insertCell(6);
+          var cell8 = row.insertCell(7);
+          var cell9 = row.insertCell(8);
+          var cell10 = row.insertCell(9);
+          cell1.innerHTML = newItem.Part;
+          cell2.innerHTML = newItem.Sourcing.Cost;
+          cell3.innerHTML = newItem.Sourcing.Inventory;
+          cell4.innerHTML = '<a href ='+newItem.Sourcing.Link+' style="text-decoration:none"> <button class="btn btn-secondary">Order</button></a>'
         });
       });
       break;
@@ -112,11 +109,12 @@ function bottleneckLogic(){
   // bottleneck code here!
   // load all relevant firebase refs, iterate through to detect "bottlenecks"
   // if bottleneck is detected, show user in the appropriate widget
+  // console.log("go here");
 
   // INVENTORY BOTTLENECK CODE
   var itemsRef = new Firebase('https://square1.firebaseio.com');
   itemsRef.once("value", function(snapshot){
-
+   // table html
     s = '<div class= "table-responsive" style = "textAlign:left">' +
           '<table class="table">' + 
             '<thead>' +  
@@ -134,8 +132,9 @@ function bottleneckLogic(){
          makeInventoryAlert(k);
       }
     });
-
+     
     s += '</tbody> </table> </div>';
+
     document.getElementById("inventory-panel-body").innerHTML = s;
   });
   // do something
