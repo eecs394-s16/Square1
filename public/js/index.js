be37d0a1ba87fc954eb72c6a58cd28f6b7718e4e
Tony Lee <chunglee2018@u.northwestern.edu>
2016-05-18 20:56:03 -0500

    format

--- public/js/index.js
+++ public/js/index.js
@@ -51,71 +51,66 @@ function loadFirebase(id){
       bottleneckLogic();
       break;
     case "inventory":
- var inventory = new Firebase('https://395s16-test.firebaseio.com/inventory');
+      var inventory = new Firebase('https://395s16-test.firebaseio.com/inventory');
 
-var bottleneck =false
-var status = "foo green"
-document.getElementById("new_inventory_entry").style.visibility='hidden';
-    
-inventory.on("value", function(snapshot) {
-    var table = document.getElementById("dataTable");
-    snapshot.forEach(function(data) {
-        var newItem = data.val();
-        var row = table.insertRow(0);
-        var remainingInventory = newItem.Sourcing.Inventory;
-        var reorder_level = newItem.Sourcing.ReorderLevel;
-        var reorder_threshold = reorder_level*0.1
+      var bottleneck =false
+      var status = "foo green"
+      document.getElementById("new_inventory_entry").style.visibility='hidden';
         
-        if (remainingInventory == 0) {
-            row.insertCell(0).innerHTML = '<div class ="foo wine"></div>'
-        }
-        else if (remainingInventory > reorder_level){
-            row.insertCell(0).innerHTML = '<div class ="foo green"></div>'
-        }
-        else if (remainingInventory <= 5 || remainingInventory <= reorder_threshold){
-            row.insertCell(0).innerHTML = '<div class ="foo yellow"></div>'
+      inventory.on("value", function(snapshot) {
+        var table = document.getElementById("dataTable");
+        snapshot.forEach(function(data) {
+            var newItem = data.val();
+            var row = table.insertRow(0);
+            var remainingInventory = newItem.Sourcing.Inventory;
+            var reorder_level = newItem.Sourcing.ReorderLevel;
+            var reorder_threshold = reorder_level*0.1
             
-        }
-        else {
-            row.insertCell(0).innerHTML = '<div class ="foo orange"></div>'
-//            document.getElementById("color").className = "foo green";
-        }
-                
-        
-        row.insertCell(1).innerHTML = newItem.Part;
-        row.insertCell(2).innerHTML=newItem.Sourcing.Inventory;
-        row.insertCell(3).innerHTML=newItem.Sourcing.ReorderLevel;
-        row.insertCell(4).innerHTML=newItem.Sourcing.Cost;
-        row.insertCell(5).innerHTML=newItem.Sourcing.Location;
-        row.insertCell(6).innerHTML = '<a href ='+newItem.Sourcing.Link+' style="text-decoration:none"> <button class="btn btn-secondary">Order</button></a>'
-
-    })
-    
-    var buttonID = document.getElementById("add_item");
-    buttonID.onclick = function(){
-        document.getElementById("new_inventory_entry").style.visibility='visible';
-    }
-
-
-    var addFirebase = document.getElementById("add_to_firebase");
-    addFirebase.onclick = function(){
-        inventory.push({
-            Part:document.getElementById("part_input").value,
-            Sourcing: {
-                Cost:document.getElementById("cost_input").value,
-                Inventory:document.getElementById("inventory_input").value,
-                ReorderLevel:document.getElementById("reorder_input").value,
-                Location:document.getElementById("location_input").value,
-                Link:document.getElementById("link_input").value
+            if (remainingInventory == 0) {
+                row.insertCell(0).innerHTML = '<div class ="foo wine"></div>'
             }
+            else if (remainingInventory > reorder_level){
+                row.insertCell(0).innerHTML = '<div class ="foo green"></div>'
+            }
+            else if (remainingInventory <= 5 || remainingInventory <= reorder_threshold){
+                row.insertCell(0).innerHTML = '<div class ="foo yellow"></div>'
+                
+            }
+            else {
+                row.insertCell(0).innerHTML = '<div class ="foo orange"></div>'
+      //            document.getElementById("color").className = "foo green";
+            }
+                    
+            
+            row.insertCell(1).innerHTML = newItem.Part;
+            row.insertCell(2).innerHTML=newItem.Sourcing.Inventory;
+            row.insertCell(3).innerHTML=newItem.Sourcing.ReorderLevel;
+            row.insertCell(4).innerHTML=newItem.Sourcing.Cost;
+            row.insertCell(5).innerHTML=newItem.Sourcing.Location;
+            row.insertCell(6).innerHTML = '<a href ='+newItem.Sourcing.Link+' style="text-decoration:none"> <button class="btn btn-secondary">Order</button></a>'
 
         })
-}
+        
+        var buttonID = document.getElementById("add_item");
+        buttonID.onclick = function(){
+            document.getElementById("new_inventory_entry").style.visibility='visible';
+        }
 
 
-    
-    
-})
+        var addFirebase = document.getElementById("add_to_firebase");
+        addFirebase.onclick = function(){
+          inventory.push({
+            Part:document.getElementById("part_input").value,
+            Sourcing: {
+              Cost:document.getElementById("cost_input").value,
+              Inventory:document.getElementById("inventory_input").value,
+              ReorderLevel:document.getElementById("reorder_input").value,
+              Location:document.getElementById("location_input").value,
+              Link:document.getElementById("link_input").value
+            }
+          })
+        }
+      })
 
 
 
@@ -210,7 +205,7 @@ function bottleneckLogic(){
   // if bottleneck is detected, show user in the appropriate widget
   // console.log("go here");
 
-  // INVENTORY BOTTLENECK CODE
+  // INVENTORY BOTTLENECK CODE ---------------------------------------------------
   var itemsRef = new Firebase('https://square1.firebaseio.com');
   itemsRef.once("value", function(snapshot){
    // table html
@@ -239,7 +234,7 @@ function bottleneckLogic(){
   // do something
   // modify widget
 
-  // SCHEDULE BOTTLENECK CODE
+  // SCHEDULE BOTTLENECK CODE  --------------------------------------------------- 
   var scheduleRef = new Firebase('https://square1.firebaseio.com/scheudle');
   // do something
   // modify widget
