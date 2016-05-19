21a0cd6a850efe7e9e04dbb3bb8855c74e269366
nix-goat <waynexun2017@u.northwestern.edu>
2016-05-19 15:38:57 -0500

    checked everything, worked on inventory

--- public/js/index.js
+++ public/js/index.js
@@ -9,31 +9,31 @@ $( document ).ready(function() {
 function load(id) {
   switch(id) {
     case "dash":
-
-    $("#page-wrapper").load('pages/dash.html', function(data) {
-      $('#inventory-panel').load('pages/inventory-widget.html');
-      $('#schedule-panel').load('pages/schedule-widget.html');
-      loadFirebase(id);
-    });
-    break;
-
+      $("#page-wrapper").load('pages/dash.html', function(data) {
+        $('#inventory-panel').load('pages/inventory-widget.html', function(){
+          $('#schedule-panel').load('pages/schedule-widget.html', function(){
+            loadFirebase(id);
+          });
+        });
+      });
+      break;
     case "inventory":
-
-    $('#page-wrapper').load('pages/inventory.html');
-    loadFirebase(id);
-    break;
+      $('#page-wrapper').load('pages/inventory.html', function(data){
+        loadFirebase(id);
+      });
+      break;
     case "schedule":
-    jQuery.get('pages/schedule.html', function(data) {
-      document.getElementById("page-wrapper").innerHTML = data;
-      loadFirebase(id);
-    });
-    break;
+      jQuery.get('pages/schedule.html', function(data) {
+        document.getElementById("page-wrapper").innerHTML = data;
+        loadFirebase(id);
+      });
+      break;
     case "shipping":
-    $('#page-wrapper').load('pages/shipping.html');
-    loadFirebase(id);
-    break;
+      $('#page-wrapper').load('pages/shipping.html');
+      loadFirebase(id);
+      break;
     default:
-    console.log("got bad load id");
+      console.log("got bad load id");
   }
 }
 
@@ -52,18 +52,17 @@ function loadFirebase(id){
       bottleneckLogic();
       break;
     case "inventory":
-      var inventory = new Firebase('https://395s16-test.firebaseio.com/inventory');
+      var inventoryRef = new Firebase('https://square1.firebaseio.com/inventory');
 
       var bottleneck =false
       var status = "foo green"
-      document.getElementById("new_inventory_entry").style.visibility='hidden';
 
-      inventory.on("value", function(snapshot) {
+      inventoryRef.on("value", function(snapshot) {
         var table = document.getElementById("dataTable");
         snapshot.forEach(function(data) {
           var newItem = data.val();
           var row = table.insertRow(0);
-          var remainingInventory = newItem.Sourcing.Inventory;
+          var remainingInventory = newItem.Sourcing.Quantity;
           var reorder_level = newItem.Sourcing.ReorderLevel;
           var reorder_threshold = reorder_level*0.1
 
@@ -75,40 +74,39 @@ function loadFirebase(id){
           }
           else if (remainingInventory <= 5 || remainingInventory <= reorder_threshold){
             row.insertCell(0).innerHTML = '<div class ="foo yellow"></div>'
-
           }
           else {
             row.insertCell(0).innerHTML = '<div class ="foo orange"></div>'
           }
 
-
           row.insertCell(1).innerHTML = newItem.Part;
-          row.insertCell(2).innerHTML=newItem.Sourcing.Inventory;
-          row.insertCell(3).innerHTML=newItem.Sourcing.ReorderLevel;
-          row.insertCell(4).innerHTML=newItem.Sourcing.Cost;
-          row.insertCell(5).innerHTML=newItem.Sourcing.Location;
+          row.insertCell(2).innerHTML = newItem.Sourcing.Quantity;
+          row.insertCell(3).innerHTML = newItem.Sourcing.ReorderLevel;
+          row.insertCell(4).innerHTML = newItem.Sourcing.Cost;
+          row.insertCell(5).innerHTML = newItem.Sourcing.Location;
           row.insertCell(6).innerHTML = '<a href ='+newItem.Sourcing.Link+' style="text-decoration:none"> <button class="btn btn-secondary">Order</button></a>'
         })
-
-        var buttonID = document.getElementById("add_item");
-        buttonID.onclick = function(){
-          document.getElementById("new_inventory_entry").style.visibility='visible';
-        }
-
-        var addFirebase = document.getElementById("add_to_firebase");
-        addFirebase.onclick = function(){
-          inventory.push({
-            Part:document.getElementById("part_input").value,
-            Sourcing: {
-              Cost:document.getElementById("cost_input").value,
-              Inventory:document.getElementById("inventory_input").value,
-              ReorderLevel:document.getElementById("reorder_input").value,
-              Location:document.getElementById("location_input").value,
-              Link:document.getElementById("link_input").value
-            }
-          })
-        }
       })
+
+      document.getElementById("new_inventory_entry").style.display='none';
+      var buttonID = document.getElementById("add_item");
+      buttonID.onclick = function(){
+        document.getElementById("new_inventory_entry").style.display='block';
+      }
+
+      var addFirebase = document.getElementById("add_to_firebase");
+      addFirebase.onclick = function(){
+        inventoryRef.push({
+          Part:document.getElementById("part_input").value,
+          Sourcing: {
+            "Cost":         document.getElementById("cost_input").value,
+            "Quantity":     document.getElementById("quantity_input").value,
+            "ReorderLevel": document.getElementById("reorder_input").value,
+            "Location":     document.getElementById("location_input").value,
+            "Link":         document.getElementById("link_input").value
+          }
+        })
+      }
       break;
     case "schedule":
       // -------------------------------------------------------------------------
@@ -120,9 +118,9 @@ function loadFirebase(id){
       // -------------------------------------------------------------------------
       // SHIPPING
       // -------------------------------------------------------------------------
-      var myFirebaseRef = new Firebase('https://square1.firebaseio.com/orders');
+      var ordersRef = new Firebase('https://square1.firebaseio.com/orders');
 
-      myFirebaseRef.set({
+      ordersRef.set({
         "2304" : {
           Order: "2304",
           Name: "John Doe",
@@ -141,7 +139,7 @@ function loadFirebase(id){
         }
       });
 
-      myFirebaseRef.on("value", function(snapshot) {
+      ordersRef.on("value", function(snapshot) {
         var table = document.getElementById("dataTable");
         snapshot.forEach(function(data) {
           var newItem = data.val();
@@ -181,7 +179,7 @@ function bottleneckLogic(){
   // console.log("go here");
 
   // INVENTORY BOTTLENECK CODE ---------------------------------------------------
-  var itemsRef = new Firebase('https://square1.firebaseio.com');
+  var itemsRef = new Firebase('https://square1.firebaseio.com/inventory');
   itemsRef.once("value", function(snapshot){
     // table html
     s = '<div class= "table-responsive" style = "textAlign:left">' +
@@ -189,7 +187,7 @@ function bottleneckLogic(){
     '<thead>' +  
     '<tr> ' +
     '<th> Part/Material</th>' + 
-    '<th> Inventory</th> ' +
+    '<th> Quantity</th> ' +
     '</tr> ' +
     '</thead>' + 
     '<tbody>' ;
@@ -197,8 +195,8 @@ function bottleneckLogic(){
     snapshot.forEach(function(data) {
       k = data.val();
       // table to show inventory alert data 
-      if (k.Sourcing && k.Sourcing.Inventory && k.Sourcing.Inventory < 20){
-        s = makeInventoryAlert(s, k);
+      if (k.Sourcing && k.Sourcing.Quantity && k.Sourcing.Quantity < 20){
+        s += makeInventoryAlert(k);
       }
     });
 
@@ -208,15 +206,16 @@ function bottleneckLogic(){
   });
 
   // SCHEDULE BOTTLENECK CODE  --------------------------------------------------- 
-  var scheduleRef = new Firebase('https://square1.firebaseio.com/scheudle');
+  var scheduleRef = new Firebase('https://square1.firebaseio.com/schedule');
   // do something
   // modify widget
 
   // bottleneck functions ----------------------
   // function for generating inventory bottleneck notification html
-  function makeInventoryAlert(s, k){
-    s += "<tr> <td> <b>" + String(k.Part) + " </b> </td>" 
-    s +=  "<td> <b> <span style='color : red'>" + String(k.Sourcing.Inventory) + "</span> </b> </td> <tr>";
-    return s;
+  function makeInventoryAlert(k){
+    a = "";
+    a += "<tr> <td> <b>" + String(k.Part) + " </b> </td>" 
+    a +=  "<td> <b> <span style='color : red'>" + String(k.Sourcing.Quantity) + "</span> </b> </td> <tr>";
+    return a;
   }
 }
