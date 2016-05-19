1091a382cba35a63d8f101656c212d65053a9525
nix-goat <waynexun2017@u.northwestern.edu>
2016-05-18 19:42:48 -0500

    moved master 4 pages for 2 features into dash, going to force overwrite

---
+++ public/js/inventory.js
@@ -0,0 +1,133 @@
+var inventory = new Firebase('https://395s16-test.firebaseio.com/inventory');
+
+//inventory.set({
+//  0: {
+//  	Part: "Wood Panel", 
+//    Sourcing : {
+//   		Cost: "20",
+//    	Inventory: "0",
+//        ReorderLevel:"50",
+//        Location:"Bin 29/Shelf 2",
+//      Link:"http://www.joann.com/plywood-sheet-12inx.25inx12in/10177616.html?mkwid=Ispx7X3s%7Cdc&utm_source=google&utm_medium=cpc&utm_term=&utm_campaign=Shopping+-+Crafts&CS_003=10131488&CS_010=%5BProductId%5D&gclid=CLGs1PrDycwCFQ8vaQodC_oBug"
+//    	// Order: "False" 
+//    }
+//  },
+//
+//  1: {
+//  	Part: "Battery",
+//    Sourcing : {
+//    	Cost: "7.3" ,
+//    	Inventory: "1" ,
+//        ReorderLevel:"30",
+//        Location:"Bin 4/Shelf 3",
+//      Link:"http://www.batteryjunction.com/energizer-a27bpz.html?gclid=CJbD647EycwCFQQbaQodKocPgw"
+//    	// Order: "False" 
+//    }
+//  },
+//
+//  2: {
+//
+//    Part: "Control Switch Panel", 
+//    Sourcing : {
+//      Cost: "4",
+//      Inventory: "12",
+//      ReorderLevel:"22",
+//      Location:"Bin 20/ Shelf 5",
+//      Link:"http://www.htd.com/Products/Speaker-Selectors/SS41-Speaker-Selector?gclid=CN71vd3EycwCFQ4zaQodToMPhA"
+//      // Order: "False" 
+//    }
+//  },
+//
+// 3: {
+//
+//    Part: "Transducer", 
+//    Sourcing : {
+//      Cost: "6",
+//      Inventory: "26",
+//      ReorderLevel:"20",
+//      Location:"Bin 15/Shelf 4",
+//      Link:"http://www.digikey.com/product-detail/en/CEB-20D64/102-1126-ND/412385?WT.mc_id=IQ_7595_G_pla412385&wt.srch=1&wt.medium=cpc&WT.srch=1&gclid=CLO14ZLFycwCFQsDaQodFdsP7A"
+//      // Order: "False" 
+//    }
+//  },
+//
+//
+//
+//  4: {
+//	 Part: "Bluetooth Chip",
+//     Sourcing: {
+//    	Cost: "3",
+//    	Inventory: "12",
+//        ReorderLevel: "15",
+//        Location: "Bin 75/Shelf 3",
+//        Link:"https://www.adafruit.com/products/1697?gclid=CKXBhKjEycwCFZWFaQod1CUJAw"
+//    	// Order: "False" 
+//    }
+//   }
+//});
+
+var bottleneck =false
+var status = "foo green"
+document.getElementById("new_inventory_entry").style.visibility='hidden';
+    
+inventory.on("value", function(snapshot) {
+    var table = document.getElementById("dataTable");
+    snapshot.forEach(function(data) {
+        var newItem = data.val();
+        var row = table.insertRow(0);
+        var remainingInventory = newItem.Sourcing.Inventory;
+        var reorder_level = newItem.Sourcing.ReorderLevel;
+        var reorder_threshold = reorder_level*0.1
+        
+        if (remainingInventory == 0) {
+            row.insertCell(0).innerHTML = '<div class ="foo wine"></div>'
+        }
+        else if (remainingInventory > reorder_level){
+            row.insertCell(0).innerHTML = '<div class ="foo green"></div>'
+        }
+        else if (remainingInventory <= 5 || remainingInventory <= reorder_threshold){
+            row.insertCell(0).innerHTML = '<div class ="foo yellow"></div>'
+            
+        }
+        else {
+            row.insertCell(0).innerHTML = '<div class ="foo orange"></div>'
+//            document.getElementById("color").className = "foo green";
+        }
+                
+        
+        row.insertCell(1).innerHTML = newItem.Part;
+        row.insertCell(2).innerHTML=newItem.Sourcing.Inventory;
+        row.insertCell(3).innerHTML=newItem.Sourcing.ReorderLevel;
+        row.insertCell(4).innerHTML=newItem.Sourcing.Cost;
+        row.insertCell(5).innerHTML=newItem.Sourcing.Location;
+        row.insertCell(6).innerHTML = '<a href ='+newItem.Sourcing.Link+' style="text-decoration:none"> <button class="btn btn-secondary">Order</button></a>'
+
+    })
+    
+    var buttonID = document.getElementById("add_item");
+    buttonID.onclick = function(){
+        document.getElementById("new_inventory_entry").style.visibility='visible';
+    }
+
+
+    var addFirebase = document.getElementById("add_to_firebase");
+    addFirebase.onclick = function(){
+        inventory.push({
+            Part:document.getElementById("part_input").value,
+            Sourcing: {
+                Cost:document.getElementById("cost_input").value,
+                Inventory:document.getElementById("inventory_input").value,
+                ReorderLevel:document.getElementById("reorder_input").value,
+                Location:document.getElementById("location_input").value,
+                Link:document.getElementById("link_input").value
+            }
+
+        })
+}
+
+
+    
+    
+})
+
+
---
+++ public/js/shipping.js
@@ -0,0 +1,50 @@
+var myFirebaseRef = new Firebase('https://square1.firebaseio.com/orders');
+
+myFirebaseRef.set({
+	"2304" : {
+		Order: "2304",
+	  	Name: "John Doe",
+	  	location: { 
+	  		Address: "1234 Apple rd",
+	  		City: "Evanston",
+	  		State: "Il",
+	  		zip: 60201
+	  	},
+	  	product: {
+	  		items:"duo speaker",
+	  		weight: "3.3 lbs"
+	  	},   
+	  	deadline: 120416,
+	  	ship: "ship"
+  }
+});
+
+myFirebaseRef.on("value", function(snapshot) {
+	var table = document.getElementById("dataTable");
+	snapshot.forEach(function(data) {
+	var newItem = data.val();
+	var row = table.insertRow(0);
+	// table.setAttribute("align","center");
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	var cell5 = row.insertCell(4);
+	var cell6 = row.insertCell(5);
+	var cell7 = row.insertCell(6);
+	var cell8 = row.insertCell(7);
+	var cell9 = row.insertCell(8);
+	var cell10 = row.insertCell(9);
+	cell1.innerHTML = newItem.Order;
+	cell2.innerHTML = newItem.Name;
+	cell3.innerHTML = newItem.location.Address;
+	cell4.innerHTML = newItem.product.items;
+	cell5.innerHTML =  newItem.product.weight;
+	cell6.innerHTML =  newItem.deadline;
+	cell7.innerHTML =  newItem.ship;
+	//cell8.innerHTML =  newItem.ship;
+
+  });
+
+
+});
