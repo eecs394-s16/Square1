6db90a31988bbb48a4c34db68dc37293e35f6628
nix-goat <waynexun2017@u.northwestern.edu>
2016-05-28 21:35:25 -0500

    UI in place for login and signup

--- public/js/splash.js
+++ public/js/splash.js
@@ -1,3 +1,43 @@
-login = function(e){
-	alert("logging in");
+// onload, load login
+$( document ).ready(function() {
+	load("login");
+});
+
+// loads pages by redrawing content DOM
+function load(id) {
+	switch(id) {
+		case "login":
+			$("#content").load('pages/login.html', function(data) {
+				//
+			});
+			break;
+		case "sign-up":
+			$("#content").load('pages/sign-up.html', function(data) {
+				//
+			});
+			break;
+		default:
+			console.log("got bad load id");
+	}
+}	
+
+driver = {
+	login:
+		// login page -> dashboard
+		// log the user in and take them to their dashboard
+		function(e){
+			alert("logging in");
+		},
+	getstarted:
+		// login page -> newuser page
+		// reload html page content with new user
+		function(e){
+			load("sign-up");
+		},
+	newuser:
+		// register page -> login page
+		// register the user and then take them back to the login page
+		function(e){
+			alert("Signed up! Now sign in");
+		},
 }
